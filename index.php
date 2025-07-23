afa1bdc7c7923552b40b652e7a964ddc81ff33f9
ai-joshua <joshuapaderan4@gmail.com>
2025-07-23 22:35:19 +0800

    restaurant website

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <br>
         <br>
     <div>
-        <h1 class="about">Immerse yourself in the artistry of our kitchen, where each dish is meticulously crafted by our passionate chefs. We source the finest ingredients, handpicked from local purveyors and global markets, ensuring that every bite ignites a culinary journey of delight.</h1>
+        <h1 class="about"> Immerse yourself in the artistry of our kitchen, where each dish is meticulously crafted by our passionate chefs. We source the finest ingredients, handpicked from local purveyors and global markets, ensuring that every bite ignites a culinary journey of delight.</h1>
     </div>
     </div>
 </section>
